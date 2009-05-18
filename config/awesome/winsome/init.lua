339ac2b3ef62c1a6729004fe3bd298d7eca4f789
elliottcable <git@elliottcable.name>
2009-05-18 12:38:34 -0800

    Moving most of the wool theme into the winsome defaults, because most of them make more sense.

--- config/awesome/winsome/init.lua
+++ config/awesome/winsome/init.lua
@@ -1,6 +1,7 @@
 winsome = {}
 
 winsome.dir = awful.util.getdir("config") .. "/winsome/"
+winsome.icons_dir = winsome.dir .. "icons/"
 winsome.awesome_dir = "/usr/share/awesome/"
 winsome.default_dir = winsome.awesome_dir .. "themes/default/"
 
@@ -8,7 +9,7 @@ winsome.theme = function(name)
   local theme = {}
   theme.name = name
 
-  -- We assume themes will be installed as ~/.config/awesome/themes/theme_name
+  -- We assume theme will be installed as ~/.config/awesome/theme/theme_name
   theme.dir = awful.util.getdir("config") .. "/themes/" .. theme.name .. "/"
   theme.shared_dir = winsome.shared_dir
   theme.default_dir = winsome.default_dir
@@ -31,42 +32,44 @@ winsome.defaults.fg_focus      = "#000000"
 winsome.defaults.fg_urgent     = "#000000"
 winsome.defaults.fg_minimize   = "#000000"
 
-winsome.defaults.border_width  = "1"
+winsome.defaults.border_width  = "0"
 winsome.defaults.border_normal = "#444444"
 winsome.defaults.border_focus  = "#000000"
 winsome.defaults.border_marked = "#005500"
 
-winsome.defaults.taglist_squares_sel = winsome.default_dir .. "taglist/squarefw.png"
-winsome.defaults.taglist_squares_unsel = winsome.default_dir .. "taglist/squarew.png"
+winsome.defaults.menu_height   = "16"
+winsome.defaults.menu_width    = "128"
 
-winsome.defaults.tasklist_floating_icon = winsome.default_dir .. "tasklist/floatingw.png"
+-- Most of these are from the "Silk" set. See the README for licensing information.
+winsome.defaults.taglist_squares_sel   = winsome.icons_dir .. "taglist_bullet_black.png"
+winsome.defaults.taglist_squares_unsel = winsome.icons_dir .. "taglist_bullet_white.png"
 
-winsome.defaults.menu_submenu_icon = winsome.default_dir .. "submenu.png"
-winsome.defaults.menu_height   = "15"
-winsome.defaults.menu_width    = "100"
+winsome.defaults.tasklist_floating_icon = winsome.icons_dir .. "floating.png"
 
-winsome.defaults.titlebar_close_button_normal = winsome.default_dir .. "titlebar/close_normal.png"
-winsome.defaults.titlebar_close_button_focus = winsome.default_dir .. "titlebar/close_focus.png"
+winsome.defaults.menu_submenu_icon = winsome.icons_dir .. "expand_submenu.png"
 
-winsome.defaults.titlebar_ontop_button_normal_inactive = winsome.default_dir .. "titlebar/ontop_normal_inactive.png"
-winsome.defaults.titlebar_ontop_button_focus_inactive = winsome.default_dir .. "titlebar/ontop_focus_inactive.png"
-winsome.defaults.titlebar_ontop_button_normal_active = winsome.default_dir .. "titlebar/ontop_normal_active.png"
-winsome.defaults.titlebar_ontop_button_focus_active = winsome.default_dir .. "titlebar/ontop_focus_active.png"
+winsome.defaults.titlebar_close_button_normal = winsome.icons_dir .. "close.png"
+winsome.defaults.titlebar_close_button_focus  = winsome.defaults.titlebar_close_button_normal
 
-winsome.defaults.titlebar_sticky_button_normal_inactive = winsome.default_dir .. "titlebar/sticky_normal_inactive.png"
-winsome.defaults.titlebar_sticky_button_focus_inactive = winsome.default_dir .. "titlebar/sticky_focus_inactive.png"
-winsome.defaults.titlebar_sticky_button_normal_active = winsome.default_dir .. "titlebar/sticky_normal_active.png"
-winsome.defaults.titlebar_sticky_button_focus_active = winsome.default_dir .. "titlebar/sticky_focus_active.png"
+winsome.defaults.titlebar_ontop_button_normal_active   = winsome.icons_dir .. "ontop.png"
+winsome.defaults.titlebar_ontop_button_normal_inactive = winsome.icons_dir .. "not_ontop.png"
+winsome.defaults.titlebar_ontop_button_focus_active    = winsome.defaults.titlebar_ontop_button_normal_active
+winsome.defaults.titlebar_ontop_button_focus_inactive  = winsome.defaults.titlebar_ontop_button_normal_inactive
 
-winsome.defaults.titlebar_floating_button_normal_inactive = winsome.default_dir .. "titlebar/floating_normal_inactive.png"
-winsome.defaults.titlebar_floating_button_focus_inactive = winsome.default_dir .. "titlebar/floating_focus_inactive.png"
-winsome.defaults.titlebar_floating_button_normal_active = winsome.default_dir .. "titlebar/floating_normal_active.png"
-winsome.defaults.titlebar_floating_button_focus_active = winsome.default_dir .. "titlebar/floating_focus_active.png"
+winsome.defaults.titlebar_sticky_button_normal_active   = winsome.icons_dir .. "sticky.png"
+winsome.defaults.titlebar_sticky_button_normal_inactive = winsome.icons_dir .. "not_sticky.png"
+winsome.defaults.titlebar_sticky_button_focus_active    = winsome.defaults.titlebar_sticky_button_normal_active
+winsome.defaults.titlebar_sticky_button_focus_inactive  = winsome.defaults.titlebar_sticky_button_normal_inactive
 
-winsome.defaults.titlebar_maximized_button_normal_inactive = winsome.default_dir .. "titlebar/maximized_normal_inactive.png"
-winsome.defaults.titlebar_maximized_button_focus_inactive = winsome.default_dir .. "titlebar/maximized_focus_inactive.png"
-winsome.defaults.titlebar_maximized_button_normal_active = winsome.default_dir .. "titlebar/maximized_normal_active.png"
-winsome.defaults.titlebar_maximized_button_focus_active = winsome.default_dir .. "titlebar/maximized_focus_active.png"
+winsome.defaults.titlebar_maximized_button_normal_active   = winsome.icons_dir .. "maximized.png"
+winsome.defaults.titlebar_maximized_button_normal_inactive = winsome.icons_dir .. "not_maximized.png"
+winsome.defaults.titlebar_maximized_button_focus_active    = winsome.defaults.titlebar_maximized_button_normal_active
+winsome.defaults.titlebar_maximized_button_focus_inactive  = winsome.defaults.titlebar_maximized_button_normal_inactive
+
+winsome.defaults.titlebar_floating_button_normal_active   = winsome.icons_dir .. "floating.png"
+winsome.defaults.titlebar_floating_button_normal_inactive = winsome.icons_dir .. "not_floating.png"
+winsome.defaults.titlebar_floating_button_focus_active    = winsome.defaults.titlebar_floating_button_normal_active
+winsome.defaults.titlebar_floating_button_focus_inactive  = winsome.defaults.titlebar_floating_button_normal_inactive
 
 winsome.defaults.layouts_dir = winsome.default_dir .. "layouts/"
 winsome.defaults.layout_fairh = winsome.defaults.layouts_dir .. "fairhw.png"
