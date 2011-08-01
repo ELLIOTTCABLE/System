d8b4e928be9b27ba06d9dc512c82aacfd77908ac
elliottcable <me@ell.io>
2011-08-01 09:39:41 +0000

    (new) UJD timestamps in irssi!

---
+++ irssi/scripts/autorun/ujd_time.pl
@@ -0,0 +1,53 @@
+# This script quite simply adds UJD (http://yreality.net/UJD) timestamps to irssi.
+
+# It provides two configuration settings: ujd_integral_digits and ujd_fractional_digits.
+
+# You might want to modify the timeout at the bottom of this file (it’s in milliseconds)
+# if this uses too much CPU for your tastes.
+
+use Irssi;
+use Irssi::TextUI;
+
+use Time::HiRes 'time';
+
+
+use vars qw($VERSION %IRSSI);
+
+$VERSION="1";
+%IRSSI = (
+  authors       => 'elliottcable'
+, contact       => 'ujd_time.pl@elliottcable.name'
+, name          => 'ujd_time'
+, description   => 'Prints the timestamp in UJD format'
+, license       => 'Public Domain'
+, url           => 'http://gist.github.com/1082336'
+);
+
+my $old_timestamp_format = Irssi::settings_get_str('timestamp_format');
+
+sub update_ujd {
+  my $integral_digits = Irssi::settings_get_int('ujd_integral_digits');
+  my $fractional_digits = Irssi::settings_get_int('ujd_fractional_digits');
+  my $time = time / 86400;
+  
+  my ($integral, $fractional) = split(/\./, sprintf("%.0${fractional_digits}f", $time));
+  my $integral = $integral % (10 ** $integral_digits);
+  
+  my $fractional = join(' ', $fractional =~ /\d{1,3}/g);
+  my $integral = reverse(join(' ', reverse($integral) =~ /\d{1,3}/g));
+  
+  
+  Irssi::command("^set timestamp_format $integral ſ $fractional");
+  Irssi::statusbar_items_redraw('time');
+}
+
+sub script_unload {
+  my ($script,$server,$witem) = @_;
+  Irssi::command("^set timestamp_format $old_timestamp_format");
+}
+
+Irssi::settings_add_int('misc', 'ujd_integral_digits', 1);
+Irssi::settings_add_int('misc', 'ujd_fractional_digits', 6);
+
+Irssi::timeout_add(23, 'update_ujd', undef);
+Irssi::signal_add_first('command script unload', 'script_unload');
