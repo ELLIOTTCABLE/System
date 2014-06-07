7d6ace91d995a6ad5cb6bcec598fa3f20ff3d0df
ELLIOTTCABLE <me@ell.io>
2014-06-07 07:15:55 -0500

    (new) Adding the first Windows stuff!
    Some AutoHotKey script to swap my keys around so shortcuts feel more
    Mac-y, and a PowerShell configuration for happiness.

---
+++ Windows/MacSwap.ahk
@@ -0,0 +1,8 @@
+; "Windows" key? What's that shit?
+RCtrl::RWin
+RWin::RCtrl
+LCtrl::LWin
+LWin::LCtrl
+
+; vim-lover
+Capslock::Esc
---
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,4 @@
+Clear-Host
+Set-Location -Path 'C:\Users\ELLIOTTCABLE'
+
+Set-Alias subl 'C:\Program Files\Sublime Text 3\sublime_text.exe'
