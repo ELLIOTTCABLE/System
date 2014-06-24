a9a3d92167c43a5bd76f9df08da31fec3868460e
ELLIOTTCABLE <me@ell.io>
2014-06-24 14:42:54 -0500

    (- Win new) Setting the PowerShell prompt

--- Windows/Microsoft.PowerShell_profile.ps1
+++ Windows/Microsoft.PowerShell_profile.ps1
@@ -2,3 +2,8 @@ Clear-Host
 Set-Location -Path 'C:\Users\ELLIOTTCABLE'
 
 Set-Alias subl 'C:\Program Files\Sublime Text 3\sublime_text.exe'
+
+function Prompt {
+    Write-Host (">") -nonewline -foregroundcolor Green
+    return " "
+}
