debc97ca31d4d1ffea6d01d1b5e3050e686b5d16
ELLIOTTCABLE <me@ell.io>
2017-05-19 23:32:48 -0500

    (!! new re zsh plug) Replacing zgen with zplug, various zsh tweaks

---
+++ Dotfiles/zsh/packages.zsh
@@ -0,0 +1,47 @@
+zplug "zplug/zplug", hook-build:'zplug --self-manage'
+
+# Steal omz's termsupport for Apple Terminal. (Hacky as fuck; will have to watch to see if this
+# changes.)
+zplug lib/functions,          from:oh-my-zsh
+zplug lib/termsupport,        from:oh-my-zsh, on:lib/functions
+
+# N.B.: the original `zsh-syntax-highlighting` had to be loaded *last*; I don't know if this is
+# still true for `fast-syntax-highlighting`
+zplug zdharma/fast-syntax-highlighting, defer:2
+#zplug zsh-users/zsh-history-substring-search
+
+# `fzf`'s built-in zsh support (uses star-star-tab, by default.)
+zplug junegunn/fzf, use:"shell/*.zsh"
+
+zplug zsh-users/zsh-autosuggestions
+
+# A zsh-contrib tool for navigating *recent* directories — included in anyframe.
+#zplug willghatch/zsh-cdr
+#zplug mollifier/anyframe, on:willghatch/zsh-cdr, if:"(( $+commands[fzf] ))"
+
+# `deer` is a file navigator for zsh heavily inspired by “ranger.”
+zplug Vifon/deer, use:"deer"
+
+# “k is a zsh script / plugin to make directory listings more readable, adding a bit of color and
+#  some git status information on files and directories.”
+zplug supercrabtree/k
+
+# These offer useful prompt/status-functions
+zplug plugins/rbenv,          from:oh-my-zsh
+
+#zplug marzocchi/zsh-notify,   if:"(( $+commands[terminal-notifier] ))"
+
+# “This plugin toggles "sudo" before the current/previous command by pressing [ESC][ESC] in emacs
+#  -mode or vi-command mode.”
+zplug hcgraf/zsh-sudo
+
+# The rest of these just offer additional completions
+zplug zsh-users/zsh-completions
+zplug akoenig/npm-run.plugin.zsh
+zplug vasyharan/zsh-brew-services
+zplug plugins/vagrant,        from:oh-my-zsh
+
+# Available, but I haven't installed them yet:
+#  - Docker
+#  - Spotify-support
+#  - nodenv
