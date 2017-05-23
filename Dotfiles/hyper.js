dd59c07eab8f6bb7b5f9d9d83fe52fe675a16dd0
ELLIOTTCABLE <me@ell.io>
2017-05-23 02:49:06 -0500

    + fixing Hyper
    (Pending henrikdahl/hyper-statusline#50, henrikdahl/hyper-tabs-enhanced#19.)

--- Dotfiles/hyper.js
+++ Dotfiles/hyper.js
@@ -16,8 +16,14 @@ module.exports = {
       // set to true for blinking cursor
       cursorBlink: true,
 
+      // color of the text
+      foregroundColor: '#fff',
+
+      // terminal background color
+      backgroundColor: '#000',
+
       // border color (window, tabs)
-      borderColor: '#FFCC66',
+      borderColor: '#333',
 
       // custom css to embed in the main window
       css: '',
@@ -48,6 +54,28 @@ module.exports = {
       hyperBorder: {
          borderWidth: '2px'
        , borderAngle: '120deg',
+      },
+
+      // the full list. if you're going to provide the full color palette,
+      // including the 6 x 6 color cubes and the grayscale map, just provide
+      // an array here instead of a color map object
+      colors: {
+        black: '#000000',
+        red: '#ff0000',
+        green: '#33ff00',
+        yellow: '#ffff00',
+        blue: '#0066ff',
+        magenta: '#cc00ff',
+        cyan: '#00ffff',
+        white: '#d0d0d0',
+        lightBlack: '#808080',
+        lightRed: '#ff0000',
+        lightGreen: '#33ff00',
+        lightYellow: '#ffff00',
+        lightBlue: '#0066ff',
+        lightMagenta: '#cc00ff',
+        lightCyan: '#00ffff',
+        lightWhite: '#ffffff'
       },
 
       // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
@@ -89,10 +117,10 @@ module.exports = {
    plugins: [
       'hypercwd'
     , 'hyperterm-alternatescroll'
-  //, 'hyperterm-focus-reporting'
+    , 'hyperterm-focus-reporting'
 
-  //, 'hyper-statusline'
-  //, 'hyper-tabs-enhanced'
+    , 'hyper-tabs-enhanced'
+    , 'hyper-statusline'
 
     , 'hypersixteen'
     , 'hyperborder'
