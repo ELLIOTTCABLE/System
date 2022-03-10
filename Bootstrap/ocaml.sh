d44076a916f4f667563be2822871f4fad7d8af58
ELLIOTTCABLE <git@ell.io>
2022-03-10 14:35:24 -0600

    (new) Automate ocaml-boostrapping

---
+++ Bootstrap/ocaml.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env dash
+
+SCRIPTPATH=$(dirname "$0")
+export OPAMYES='true' OPAMBUILDDOC='true'
+
+set -ex
+opam init --bare --no-setup
+
+opam switch create default ocaml-base-compiler
+
+# Unfortunately, opam [isn't interested in fixing this](https://github.com/ocaml/opam/issues/3512).
+# Have to manually add a *non-default path* to a ‘_globals’ switch, if I want to be able to install
+# “global binaries” with opam (like `ocamlformat` or `patdiff`.)
+#
+# The name '_globals' is chosen pretty much at random. Make sure to mirror it in Dotfiles/shenv!
+opam switch create _globals --empty --no-switch \
+  --description="System-global binaries"
+
+opam install --working-dir --deps-only --set-root --best-effort \
+  --switch=_globals \
+  "$SCRIPTPATH/../Dotfiles/opam/default-packages.opam"
