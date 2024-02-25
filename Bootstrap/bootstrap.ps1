a38fef34c80ebe6c99a70a949537965e564816dc
ELLIOTTCABLE <git@ell.io>
2024-02-24 19:05:59 -0600

    (new Win) Add more winget packages

--- Bootstrap/bootstrap.ps1
+++ Bootstrap/bootstrap.ps1
@@ -23,35 +23,46 @@ function Install-Package-Manager {
 
 Install-Package-Manager
 
-winget install -e --id Armin2208.WindowsAutoNightMode
-winget install -e --id AutoHotkey.AutoHotkey
-winget install -e --id Discord.Discord
-winget install -e --id EpicGames.EpicGamesLauncher
-winget install -e --id FinalWire.AIDA64.Extreme
+# Frontload
+winget install -e --id RandyRants.SharpKeys
+winget install -e --id AgileBits.1Password.Beta
 winget install -e --id Git.Git
 winget install -e --id GitHub.cli
 winget install -e --id GitHub.GitHubDesktop
+winget install -e --id Logitech.OptionsPlus
+winget install -e --id Mozilla.Firefox
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id SyncTrayzor.SyncTrayzor
+winget install -e --id Yubico.Authenticator
+winget install -e --id Notion.Notion
+winget install -e --id glzr-io.glazewm
+
+# Next
+winget install -e --id Valve.Steam
+winget install -e --id RescueTime.DesktopApp
+winget install -e --id Nvidia.GeForceExperience
+winget install -e --id EpicGames.EpicGamesLauncher
+winget install -e --id Ubisoft.Connect
+winget install -e --id ElectronicArts.EADesktop
+
+# Rest
+winget install -e --id Armin2208.WindowsAutoNightMode
+winget install -e --id AutoHotkey.AutoHotkey
+winget install -e --id Discord.Discord
+winget install -e --id FinalWire.AIDA64.Extreme
 winget install -e --id Google.Chrome
 winget install -e --id icsharpcode.ILSpy
 winget install -e --id IPFS.IPFS-Desktop
-winget install -e --id Logitech.OptionsPlus
 winget install -e --id Microsoft.PowerToys
 winget install -e --id Microsoft.VisualStudio.2022.Community
-winget install -e --id Microsoft.VisualStudioCode
-winget install -e --id Mozilla.Firefox
 winget install -e --id NexusMods.Vortex
-winget install -e --id Notion.Notion
-winget install -e --id Nvidia.GeForceExperience
 winget install -e --id OpenWhisperSystems.Signal
-winget install -e --id OpenWhisperSystems.Signal
-winget install -e --id RescueTime.DesktopApp
 winget install -e --id SatisfactoryModding.SatisfactoryModManager
 winget install -e --id SlackTechnologies.Slack
-winget install -e --id Spotify.Spotify
-winget install -e --id Spotify.Spotify
-winget install -e --id SyncTrayzor.SyncTrayzor
 winget install -e --id Ubisoft.Connect
 winget install -e --id VB-Audio.Voicemeeter.Banana
-winget install -e --id Yubico.Authenticator
+winget install -e --id AgileBits.1Password.CLI
+winget install -e --id code52.Carnac
 
-winget install -e --id 9PBLBM45RJQJ # Harvest Time Tracker
+# TODO: User-level (non-admin)
+winget install -e --id 9NCBCSZSJRSB # Spotify
