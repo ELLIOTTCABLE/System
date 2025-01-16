ece9f1f2429ee9b1985f36d0dc7af88f10bbc7ed
ELLIOTTCABLE <git@ell.io>
2025-01-16 16:58:37 -0600

    (- re Win) Reorganize winget stanzas

--- Bootstrap/rest.ps1
+++ Bootstrap/rest.ps1
@@ -1,29 +1,30 @@
 # Next
-winget install -e --id Valve.Steam
-winget install -e --id RescueTime.DesktopApp
-winget install -e --id Nvidia.GeForceExperience
-winget install -e --id EpicGames.EpicGamesLauncher
-winget install -e --id Ubisoft.Connect
 winget install -e --id ElectronicArts.EADesktop
+winget install -e --id EpicGames.EpicGamesLauncher
+winget install -e --id Nvidia.GeForceExperience
+winget install -e --id RescueTime.DesktopApp
+winget install -e --id Ubisoft.Connect
+winget install -e --id Valve.Steam
 
 # Rest
+winget install -e --id AgileBits.1Password.CLI
 winget install -e --id Armin2208.WindowsAutoNightMode
 winget install -e --id AutoHotkey.AutoHotkey
 winget install -e --id Discord.Discord
 winget install -e --id FinalWire.AIDA64.Extreme
 winget install -e --id Google.Chrome
-winget install -e --id icsharpcode.ILSpy
 winget install -e --id IPFS.IPFS-Desktop
 winget install -e --id Microsoft.PowerToys
 winget install -e --id Microsoft.VisualStudio.2022.Community
 winget install -e --id NexusMods.Vortex
 winget install -e --id OpenWhisperSystems.Signal
+winget install -e --id PrismLauncher.PrismLauncher
 winget install -e --id SatisfactoryModding.SatisfactoryModManager
 winget install -e --id SlackTechnologies.Slack
 winget install -e --id Ubisoft.Connect
 winget install -e --id VB-Audio.Voicemeeter.Banana
-winget install -e --id AgileBits.1Password.CLI
 winget install -e --id code52.Carnac
+winget install -e --id icsharpcode.ILSpy
 
 # TODO: User-level (non-admin)
 winget install -e --id 9NCBCSZSJRSB # Spotify
