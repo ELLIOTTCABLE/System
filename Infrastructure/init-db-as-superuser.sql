d13623d133ecae4535318946770d69c22d403c55
ELLIOTTCABLE <git@ell.io>
2025-02-08 07:19:43 -0600

    (- meta wmill) Vendor in windmill-db script

---
+++ Infrastructure/init-db-as-superuser.sql
@@ -0,0 +1,23 @@
+-- From:
+-- <https://github.com/windmill-labs/windmill/blob/main/init-db-as-superuser.sql>
+CREATE ROLE windmill_user;
+
+GRANT ALL
+ON ALL TABLES IN SCHEMA public 
+TO windmill_user;
+
+GRANT ALL PRIVILEGES 
+ON ALL SEQUENCES IN SCHEMA public 
+TO windmill_user;
+
+ALTER DEFAULT PRIVILEGES 
+    IN SCHEMA public
+    GRANT ALL ON TABLES TO windmill_user;
+
+ALTER DEFAULT PRIVILEGES 
+    IN SCHEMA public
+    GRANT ALL ON SEQUENCES TO windmill_user;
+
+
+CREATE ROLE windmill_admin WITH BYPASSRLS;
+GRANT windmill_user TO windmill_admin;
