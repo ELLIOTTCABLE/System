1a069b36e4ea556fa7df001b409551ee9775dc2d
ELLIOTTCABLE <git@ell.io>
2025-02-21 14:13:01 -0600

    (new Mac dep) Add NoTunes to open Spotify instead

--- Bootstrap/bootstrap-macOS.sh
+++ Bootstrap/bootstrap-macOS.sh
@@ -1,1 +1,6 @@
 brew services start "borders" # https://github.com/FelixKratz/JankyBorders
+
+# Replace iTunes w/ Spotify; requires `brew install --cask notunes` from `rest.brewfile`.
+# (See: <https://github.com/tombonez/noTunes>)
+defaults write digital.twisted.noTunes replacement "/Applications/Spotify.app"
+defaults write digital.twisted.noTunes hideIcon 1
