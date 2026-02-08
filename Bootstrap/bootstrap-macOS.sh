ac03d65d110a2f10f9181941a57b36f8c748f767
ELLIOTTCABLE <git@ell.io>
2026-02-08 16:40:54 -0600

    (- new Mac) Add a few SSBs I use to macOS-bootstrap

--- Bootstrap/bootstrap-macOS.sh
+++ Bootstrap/bootstrap-macOS.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env sh
+# shellcheck shell=dash
+set -eu
+cd "$(dirname "$0")/.." || exit 1
+
 brew services start "borders" # https://github.com/FelixKratz/JankyBorders
 
 # Replace iTunes w/ Spotify; requires `brew install --cask notunes` from `rest.brewfile`
@@ -14,3 +19,17 @@ defaults write com.apple.dock expose-group-apps -bool true
 
 # TODO: Tweak this to not include additional paths from mise etc
 sudo launchctl config user path "${PATH}"
+
+# Create Safari SSBs
+printf "Use GUI-automation to create Safari web-apps? (Do not intreact with system until complete!) [yN] "
+read -r yn
+case $yn in
+   [Yy]*) ;;
+       *) exit 1;;
+esac
+
+swift ./Scripts/create-safari-webapp.swift --verbose --yes "Perplexity" "https://www.perplexity.ai/"
+swift ./Scripts/create-safari-webapp.swift --verbose --yes "Proton Mail" "https://mail.proton.me/"
+swift ./Scripts/create-safari-webapp.swift --verbose --yes "FactorioLab" "https://factoriolab.github.io/"
+
+printf "Done! Please log out and back in for all changes to take effect.\n"
