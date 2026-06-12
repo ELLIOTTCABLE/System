51c0c9bb6fad2ef4226296305d12c6eaeec639ca
ELLIOTTCABLE <git@ell.io>
2026-06-12 08:42:28 -0500

    (AI new bin) Add CLI syncthing-diagnose tool

--- Bootstrap/frontload.ps1
+++ Bootstrap/frontload.ps1
@@ -6,11 +6,11 @@ winget install -e --id AgileBits.1Password.Beta
 winget install -e --id GitHub.GitHubDesktop
 winget install -e --id GitHub.cli
 winget install -e --id Logitech.OptionsPlus
+winget install -e --id Martchus.syncthingtray
 winget install -e --id Microsoft.VisualStudioCode
 winget install -e --id Mozilla.Firefox
 winget install -e --id Notion.Notion
 winget install -e --id RandyRants.SharpKeys
-winget install -e --id SyncTrayzor.SyncTrayzor
 winget install -e --id Yubico.Authenticator
 winget install -e --id glzr-io.glazewm
 winget install -e --id jdx.mise
